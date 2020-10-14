2a372449e8e7ca13410b45adf4c6b029370a4e08
moksh <moksh@source.one>
2020-10-14 17:24:50 +0530

    adding style

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    return <div>inside the search </div>;
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image search</label>
+            <input type="text"></input>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
