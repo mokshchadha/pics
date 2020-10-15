4b9a03614fa87a0f1f9970d128f0c5fce6066bf5
moksh <moksh@source.one>
2020-10-16 02:11:12 +0530

    set up axios and making request to get data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    const res = axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: "Client-ID qO72ekR08rKg5kpzS_cn2KQTqEbgqLxAuhmYwzNYt60",
+      },
+    });
   }
   render() {
     return (
